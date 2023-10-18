965cf46d1402370fae69c768a00043beef20f960
Milki19 <mstefan558@gmail.com>
2023-10-18 05:38:35 -0700

    Gotov Vezbe1 sto se tice papira ostao je samo CRUD

--- proizvod.js
+++ proizvod.js
@@ -2,39 +2,40 @@ window.addEventListener("load", function(){
 	document.getElementById("naziv").addEventListener("keypress", function(){
 		this.classList.remove('success'); 
         this.classList.remove('error'); 
-});
+    });
 
 
-document.getElementById("forma").addEventListener("submit", function(){
-	if( document.getElementById("naziv").value.length < 3 ){
-        validno=false;
-        document.getElementById("naziv").classList.add("error");
-        document.getElementById("naziv").classList.remove("success");
-    
-      }else {
-        document.getElementById("naziv").classList.remove("error");
-        document.getElementById("naziv").classList.add("success");
-    
-    }
+    document.getElementById("forma").addEventListener("submit", function(){
+        if( document.getElementById("naziv").value.length < 3 ){
+            validno=false;
+            document.getElementById("naziv").classList.add("error");
+            document.getElementById("naziv").classList.remove("success");
+        
+        }else {
+            document.getElementById("naziv").classList.remove("error");
+            document.getElementById("naziv").classList.add("success");
+        
+        }
 
-    var spanovi = document.querySelectorAll("#sastojci > span.badge");
-    var niz = [];
-    for(let i=0; i<spanovi.length; i++){
-        niz.push(spanovi[i].dataset.id);
-     }
-     
+        var spanovi = document.querySelectorAll("#sastojci > span.badge");
+        var niz = [];
+        for(let i=0; i<spanovi.length; i++){
+            niz.push(spanovi[i].dataset.id);
+        }
+        
 
-      return validno
-});
+        return validno
+    });
 
-    document.getElementById("dodaj-sastojak").addEventListener("click", function(){
+    document.getElementById("dodaj-kombinaciju").addEventListener("click", function(){
         var id = document.getElementById("spisak-odece").value;
+        
         if(!id){
             alert("Izaberi sastojak");
             return;
         }
-    dodajSastojak( id );
-});
+        dodajSastojak( id );
+    });
 
 
 
@@ -61,7 +62,7 @@ function dodajSastojak(id){
     button.addEventListener("click", function(){
         var id = this.parentNode.dataset.id;
         this.parentNode.parentNode.removeChild( this.parentNode );
-        document.querySelector(`#spisak-sastojaka > option[value='${id}']`).disabled = false;
+        document.querySelector(`#spisak-odece > option[value='${id}']`).disabled = false;
 
     });
 
